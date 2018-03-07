b25336a110ac8d6bc646271d873d8168d9efd765
Daniel Overholt <daniel@h13.aau1x.klient.sydhavn.site.aau.dk>
2018-03-07 11:36:02 +0100

    update to support ESP32 boards OLED screens

--- examples/ESP32sendHall/ESP32OSCsendHall.ino
+++ examples/ESP32sendHall/ESP32OSCsendHall.ino
@@ -4,6 +4,7 @@
   The example is sending "hello, osc." to the address "/test".
   This example code is in the public domain.
 --------------------------------------------------------------------------------------------- */
+#define ESP32wOLED //use Arduino's "Manage Libraries" function to search and add "ESP8266 and ESP32 Oled driver for SSD1306 display"
 #define ESP32
 #if defined(ESP8266)
 #include <ESP8266WiFi.h>
@@ -12,6 +13,12 @@
 #endif
 #include <WiFiUdp.h>
 #include <OSCMessage.h>
+
+#if defined(ESP32wOLED)
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+SSD1306  display(0x3c, 5, 4);
+#endif
 
 
 WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
@@ -22,9 +29,45 @@ const char *ssid = "ESP32-OSC";             //BS SSID
 
 int hallSensor;
 
+
+void printBuffer(void) {
+  // Initialize the log buffer
+  // allocate memory to store 8 lines of text and 30 chars per line.
+  display.setLogBuffer(5, 30);
+
+  // Some test data
+  const char* test[] = {
+    "Join my network called",
+    "ESP32-OSC",
+    "---------",
+    "You can receive OSC",
+    "packets on port 9999",
+  };
+
+  for (uint8_t i = 0; i < 5; i++) {
+    display.clear();
+    // Print to the screen
+    display.println(test[i]);
+    // Draw it to the internal screen buffer
+    display.drawLogBuffer(0, 0);
+    // Display it on the screen
+    display.display();
+    delay(500);
+  }
+}
+
+
+
+
 void setup() {
     Serial.begin(115200);
-
+    #if defined(ESP32wOLED)
+    display.init();
+    display.flipScreenVertically();
+    display.setContrast(255);
+    printBuffer();
+    #endif
+    
     // Create a WiFi network
     WiFi.softAP(ssid);
     IPAddress myIP = WiFi.softAPIP();
