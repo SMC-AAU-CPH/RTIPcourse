69858fec188d58f4f48683922ee9293a4e8905c1
Pardue <punk@mit.edu>
2018-03-14 22:27:14 +0100

    Added ESP32 example.

---
+++ Wireless/ESP32_Bela_Sample.ino
@@ -0,0 +1,117 @@
+/*---------------------------------------------------------------------------------------------
+  Open Sound Control (OSC) library for the ESP8266/ESP32
+  Example for sending messages from the ESP8266/ESP32 to a remote computer
+  The example is sending "hello, osc." to the address "/test".
+  This example code is in the public domain.
+--------------------------------------------------------------------------------------------- */
+#define ESP32wOLED //use Arduino's "Manage Libraries" function to search and add "ESP8266 and ESP32 Oled driver for SSD1306 display"
+#define ESP32
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+
+#if defined(ESP32wOLED)
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+SSD1306  display(0x3c, 5, 4);
+#endif
+
+
+WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
+const IPAddress outIp(192,168,30,1);         //default IP, will change with received udp
+const unsigned int outPort = 666;          // remote port to receive OSC
+const unsigned int localPort = 8888;        // local port to listen for OSC packets (actually not used for sending)
+const char *ssid = "MyBelaAccessPoint";             //BS SSID
+const char *password = "wifipassword";
+
+
+int hallSensor;
+
+#if defined(ESP32wOLED)
+void printBuffer(void) {
+  // Initialize the log buffer
+  // allocate memory to store 8 lines of text and 30 chars per line.
+  display.setLogBuffer(5, 30);
+
+  // Some test data
+  const char* test[] = {
+    "Join my network called",
+    "ESP32-OSC",
+    "---------",
+    "You can receive OSC",
+    "packets on port 9999",
+  };
+
+  for (uint8_t i = 0; i < 5; i++) {
+    display.clear();
+    // Print to the screen
+    display.println(test[i]);
+    // Draw it to the internal screen buffer
+    display.drawLogBuffer(0, 0);
+    // Display it on the screen
+    display.display();
+    delay(500);
+  }
+}
+#endif
+
+
+
+void setup() {
+    Serial.begin(115200);
+    #if defined(ESP32wOLED)
+    display.init();
+    display.flipScreenVertically();
+    display.setContrast(255);
+    printBuffer();
+    #endif
+    
+    // Create a WiFi network
+    //WiFi.softAP(ssid);
+    //IPAddress myIP = WiFi.softAPIP();
+
+
+    WiFi.mode(WIFI_STA);
+//    WiFi.hostname("espboard");      // DHCP Hostname (useful for finding device for static lease)
+    IPAddress staticip(192, 168, 30, 10);
+    IPAddress subnet(255,255,255,0);
+    IPAddress gateway(192,168,30,1);
+    WiFi.config(staticip, gateway, subnet);  // (DNS not required)
+    WiFi.begin(ssid, password);
+    //Serial.println("IP address: ");
+    //Serial.println(myIP);
+
+//    while (WiFi.status() != WL_CONNECTED) {
+//        delay(500);
+//        Serial.println("Connecting to WiFi..");
+//    }
+    
+//    WiFi.config(ip, dns1, gateway, subnet);
+ 
+    Serial.println("Connected to the WiFi network");
+
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Sending to port: ");
+#ifdef ESP32
+    Serial.println(outPort);
+#else
+    Serial.println(Udp.outPort());
+#endif
+
+}
+
+void loop() {
+    hallSensor = hallRead();
+    OSCMessage msg("/esp"); //OSC message compilation
+    msg.add(hallSensor);
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty(); // free space occupied by message
+    delay(5);
+}
